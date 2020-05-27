0210351ed4cacecc20dff02b7034bf63073d2c4c
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-27 23:13:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,3 +26,13 @@ class CashRegister
       @to_take_off = (price * discount)/100
       @total -= @to_take_off
       return "After the discount, the total comes to $#{total}."
+    else 
+      return "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= @price
+  end
+
+end

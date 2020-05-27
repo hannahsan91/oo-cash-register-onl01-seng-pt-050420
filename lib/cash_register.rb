5cad122d2ed79c58d1068c4c7a8186e9ebade4ea
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-27 23:12:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,28 @@
 class CashRegister
   attr_accessor :total
 
-  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << items
+        counter += 1
+      end
+    else
+      @items << items
+    end
+  end
+
+  def apply_discount
+    if @discount > 0
+      @to_take_off = (price * discount)/100
+      @total -= @to_take_off
+      return "After the discount, the total comes to $#{total}."

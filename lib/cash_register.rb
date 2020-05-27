f015b93bf4b4c34f26a63b38622d44d18cd17e9b
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-27 23:57:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,39 +1,75 @@
 require 'pry'
-class CashRegister
-  attr_accessor :total, :discount, :price, :items
 
-  def initialize(discount = 0)
+class CashRegister
+
+  attr_accessor :discount, :total, :items, :basket
+
+  def initialize(employee_discount=0)
     @total = 0
-    @discount = discount
+    @employee_discount = employee_discount
     @items = []
+    @basket = []
   end
 
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
+  def discount
+    @employee_discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(name, price, quantity=1)
+    current_item = {
+      :item => name,
+      :price => price,
+      :quantity => quantity
+    }
+    @basket << current_item
+
+    if quantity != 1
+      count = quantity
+      while count > 0
+        @items << name
+        count -= 1
       end
+      adjusted_for_quantity = price * quantity
+      @total += adjusted_for_quantity
     else
-      @items << item
+      @items << name
+      @total += price
     end
   end
 
   def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
+    if @employee_discount == 0
       return "There is no discount to apply."
+    else
+      discount_by = 1.to_f - (@employee_discount.to_f/100.to_f)
+      @total = @total * discount_by
+      return "After the discount, the total comes to $#{@total.round}."
     end
   end
 
+  def items
+    @items
+  end
+
   def void_last_transaction
-    @total -= @price
+    if @basket.length == 0
+      return 0.to_f
+    else
+      amount_reduced = @basket[-1][:price]
+      if @basket[-1][:quantity] > 1
+        quantity = @basket[-1][:quantity]
+        @total -= amount_reduced * quantity
+      else
+        @total -= amount_reduced
+      end
+      @basket.delete(@basket[-1])
+      @items.delete(@items[-1])
+    end
+    @total
   end
 
 end
